0fcc92785939474fe079fce94da23f4115669614
orchief <18230373213@163.com>
2018-01-18 18:03:43 +0800

    2

---
+++ src/alipay/Pay.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Orchief\alipay;
+
+/**
+ * 支付类
+ */
+class Pay {
+  public function prepare()
+  {
+    echo "生成预订单";
+  }
+
+  public function query()
+  {
+    echo "主动查询";
+  }
+}
